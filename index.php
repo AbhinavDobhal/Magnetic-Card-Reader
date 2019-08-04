f27e48d3c7589e5cf7594f4b244b768b915fc453
Alvin Abhinav <alvin.abhinav@ithands.net>
2019-08-04 19:07:09 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<script type="text/javascript" src="jquery-1.7.2.js"></script>
+		<script type="text/javascript" src="jquery.cardswipe.js"></script>
+		<title>Magnetic Card Reader</title>
+	</head>
+	<body bgcolor="black">
+     <font color="#FFFFFF" >
+		<center>
+        	<h1>Magnetic Card Reader</h1>
+                  <hr width="50%">
+			[1] : Plug-In Your Magnetic Stripe/Card Reader in USB Port.
+			<br/>
+			<br/>
+
+<img src="card.png" alt="Card Reader" height="100" width="200">
+         <br/>
+		 <br/>
+
+            [2] : Scan/Swipe Your Card  using "Magnetic Card Reader".&nbsp;&nbsp;&nbsp;
+
+
+        </center>
+	</font>
+    
+    
+		<?php include("MySwipe.php");?>
+   
+   
+   </body>
+</html>
